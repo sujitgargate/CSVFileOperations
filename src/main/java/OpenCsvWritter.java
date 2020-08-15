ca00d5b24f248f0dca7bba460f8ded5b5970a413
sujit gargate <sujitgargate5893@gmail.com>
2020-08-15 17:54:45 +0530

    Added : Write CSV Data To Json File

--- src/main/java/OpenCsvWritter.java
+++ src/main/java/OpenCsvWritter.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OpenCsvWritter {
-   private static final String OBJECT_LIST_SAMPLE = "C:\\Users\\sujit\\Desktop\\Felloship\\OpenSourceLibraries\\src\\main\\resources\\testCSV.csv";
+   private static final String OBJECT_LIST_SAMPLE = "C:\\Users\\sujit\\Desktop\\Felloship\\OpenSourceLibraries\\src\\main\\resources\\outputCSV.csv";
 
    public static void main(String[] args) throws IOException,
            CsvDataTypeMismatchException,
